89c61b0d473535936949d6cc8456f4ed99b28bfa
4JN22CS147 <gowdasharmadar@gmail.com>
2026-03-06 16:44:24 +0530

    basic python programs

---
+++ greet.py
@@ -0,0 +1,5 @@
+class person():
+    def greet(self, name, age):
+        print("hello, my name is ",name," and my age is ",age)
+p1=person()
+p1.greet("sharmada", 21)
---
+++ oops.py
@@ -0,0 +1,8 @@
+class person():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def greet(self):
+        print("Hello, my name is " + self.name)
+p1=person("john",36)
+p1.greet()
